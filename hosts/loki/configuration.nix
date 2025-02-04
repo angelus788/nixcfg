ba37bf8bf10ff2c76d4b260a2f15123a9af28436
Emilio S <3382264+angelus788@users.noreply.github.com>
2025-02-04 16:11:21 -0500

    update

--- hosts/loki/configuration.nix
+++ hosts/loki/configuration.nix
@@ -43,8 +43,14 @@
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  # services.xserver.displayManager.gdm.enable = true;
+  # services.xserver.desktopManager.gnome.enable = true;
+
+  # Enable hyprland 
+ 	  programs.hyprland = {
+	    enable = true;
+	    xwayland.enable = true;
+	  };
 
 
   # Configure keymap in X11
