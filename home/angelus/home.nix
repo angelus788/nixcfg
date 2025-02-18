50f8369000f6bef6ec20a9e01375f979b2336a20
angelus <angelus@mac.mynetworksettings.com>
2025-02-18 10:39:30 -0500

    first commit mod

--- home/angelus/home.nix
+++ home/angelus/home.nix
@@ -22,7 +22,7 @@
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = [
-    cowsay
+    pkgs.cowsay
     # # Adds the 'hello' command to your environment. It prints a friendly
     # # "Hello, world!" when run.
     # pkgs.hello
